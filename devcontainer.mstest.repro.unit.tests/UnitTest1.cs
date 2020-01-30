722f35c128f439b111dfa8732d85136ebbf95ec9
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2020-01-30 11:12:50 -0700

    Repro project for VS Code Remote-Docker dotnet test issue.

---
+++ devcontainer.mstest.repro.unit.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace devcontainer_mstest_repro_unit_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
